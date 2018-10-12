78bac140b1fcbeda674c93de57b93d35fa898428
Aurea <fu.sakura@gmail.com>
2018-10-12 11:49:34 -0700

    implement passing brute force method

--- lib/matrix_convert_to_zero.rb
+++ lib/matrix_convert_to_zero.rb
@@ -3,5 +3,32 @@
 # If any number is found to be 0, the method updates all the numbers in the
 # corresponding row as well as the corresponding column to be 0.
 def matrix_convert_to_0(matrix)
-  raise NotImplementedError
+
+  rows = matrix.length
+  columns = matrix[0].length
+
+
+  zeros = []
+  rows.times do |row|
+    columns.times do |column|
+      if matrix[row][column] == 0
+        zeros << {:row => row, :col => column}
+      end
+    end
+  end
+
+  zeros.each do |zero_locations|
+    
+    rows.times do |row|
+      matrix[row][zero_locations[:col]] = 0
+    end
+    
+
+    columns.times do |column|
+      matrix[zero_locations[:row]][column] = 0
+    end
+    
+  end
+
+
 end
