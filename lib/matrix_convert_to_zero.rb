0903859170bb9c96b1d305bd38cb5b9a942a6453
Anibel America <ameriabel@gmail.com>
2018-10-28 18:03:53 -0700

    adds first attempt at matrix convert to zero algorithm

--- lib/matrix_convert_to_zero.rb
+++ lib/matrix_convert_to_zero.rb
@@ -3,5 +3,29 @@
 # If any number is found to be 0, the method updates all the numbers in the
 # corresponding row as well as the corresponding column to be 0.
 def matrix_convert_to_0(matrix)
-  raise NotImplementedError
+
+  rows = matrix.length
+  columns = matrix[0].length
+
+  zeroes = {}
+
+  rows.times do |r|
+    columns.times do |c|
+      if matrix[r][c] == 0
+        zeroes[r] = c
+      end
+    end
+  end
+
+  zeroes.each do |row, column|
+    rows.times do |r|
+      if row == r
+        columns.times do |num|
+          matrix[r][num] = 0
+        end
+      end
+      matrix[r][column] = 0
+    end
+  end
+
 end
