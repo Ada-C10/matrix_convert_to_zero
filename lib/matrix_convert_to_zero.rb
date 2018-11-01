9442b1eae479f0bb59930dc86f3aec100147ec68
Kay <kayxn23@gmail.com>
2018-10-31 22:53:28 -0700

    added space and time complexity which might need revision

--- lib/matrix_convert_to_zero.rb
+++ lib/matrix_convert_to_zero.rb
@@ -3,7 +3,6 @@
 # If any number is found to be 0, the method updates all the numbers in the
 # corresponding row as well as the corresponding column to be 0.
 def matrix_convert_to_0(matrix)
-  puts "beginning #{matrix}"
   (matrix.length).times do |i| #number of rows times do runs 3 times
     (matrix[i].length).times do |j| #number of columns this runs 4 times
       if matrix[i][j] == 0 #if something in this row/column is 0
@@ -18,6 +17,21 @@ def matrix_convert_to_0(matrix)
       end
     end
   end
-  puts "end #{matrix}"
   return matrix
 end
+
+# Space Complexity:
+# Regardless of the side of the matrix, we need to create two variables
+#row and column to hold the position of the element that is 0.
+# The row and column variables will only contain 1 integer each,
+#which stays the same, thefore it is Constant O(1) Space Complexity.
+#
+# Time Complexity:
+# There are 4 loops in this solution. The first loop will perform n times
+# (where n is the number of rows that the matrix contains). The second loop
+#will perform m times (where m is the number of columns the matrix contains).
+# If 0 is found in a row/column combination, in the worst case the third and fourth
+# loops will run row.length * column.length times, or n x n, or n**2.
+# Overall, the time complexity is O(n*m) + n **2
+#
+#
