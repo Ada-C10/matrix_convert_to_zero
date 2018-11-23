a0e98eed66a9974887c53a2671e77061948f37c7
Addison Cole <addison.cole85@gmail.com>
2018-11-23 11:30:04 -0800

    add method matrix_convert_to_0, which takes input of a matrix, checks each row for a value of 0, and if found, changes all values in row to 0 and all values in that column to 0.

--- lib/matrix_convert_to_zero.rb
+++ lib/matrix_convert_to_zero.rb
@@ -3,5 +3,32 @@
 # If any number is found to be 0, the method updates all the numbers in the
 # corresponding row as well as the corresponding column to be 0.
 def matrix_convert_to_0(matrix)
-  raise NotImplementedError
+  current_matrix = 0
+  current_loc = 0
+  delta = 0
+
+  while current_matrix <= (matrix.length - 1) do
+    while current_loc <= matrix[current_matrix].length - 1 do
+      if matrix[current_matrix][current_loc] == 0
+        delta = current_loc
+        delta_count = 0
+        while delta_count <= matrix[current_matrix].length - 1 do
+          matrix[current_matrix][delta_count] = 0
+          delta_count += 1
+        end
+        delta_count = 0
+        while delta_count <= matrix.length - 1 do
+          matrix[delta_count][delta] = 0
+          delta_count += 1
+        end
+        current_loc += 1
+      else
+        current_loc += 1
+      end
+    end
+    current_matrix += 1
+    current_loc = 0
+  end
+
+  return matrix
 end
