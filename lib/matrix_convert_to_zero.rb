0e51ad8947ee2e9c02ab0d65d44a36f31af5d104
shrutivanw <shruti@adadevelopersacademy.org>
2018-11-27 12:15:56 -0500

    Update matrix_convert_to_zero.rb

--- lib/matrix_convert_to_zero.rb
+++ lib/matrix_convert_to_zero.rb
@@ -2,13 +2,6 @@
 # Assumption/ Given: All numbers in the matrix are 0s or 1s
 # If any number is found to be 0, the method updates all the numbers in the
 # corresponding row as well as the corresponding column to be 0.
-def matrix_convert_to_0(matrix)
-  raise NotImplementedError
-end
-# Updates the input matrix based on the following rules:
-# Assumption/ Given: All numbers in the matrix are 0s or 1s
-# If any number is found to be 0, the method updates all the numbers in the
-# corresponding row as well as the corresponding column to be 0.
 # Time complexity: O(rows * columns) - the number of times the nested loop statements get executed
 # Space complexity: O(rows + columns) - additional storage is needed to keep track of which rows and which columns need to convert to zero
 def matrix_convert_to_0(matrix)
